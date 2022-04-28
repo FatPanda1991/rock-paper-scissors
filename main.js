7117cdf21d966f59cae8ddc36e36bd09d6f1ecd8
FatPanda1991 <egidijus8800@gmail.com>
2022-04-28 22:45:02 +0300

    Add game function for 5 games and score caunting

--- main.js
+++ main.js
@@ -4,31 +4,52 @@ function computerPlay(){
     let result = computerChoices[Math.floor(Math.random()*computerChoices.length)];
     return result;
 }
-
+playerSelection = '';
 let computerSelection = computerPlay();
-let playerSelection = prompt("Rock-Paper-Scissors").toLowerCase();
+let draw = 'Draw';
+let win = `You win ${playerSelection} beats ${computerSelection}`;
+let lost = `You lost ${computerSelection} beats ${playerSelection}`;
+let playerScore = 0;
+let computerScore = 0;
+
 
 
 function singleGame(playerSelection, computerSelection){
-    if(playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissor'){
-        console.log("WTF is that");
-    }
-    else if(playerSelection === computerSelection){
-        console.log("draw");
+    if(playerSelection === computerSelection){
+        return draw;
     }
     else if(playerSelection === 'rock' && computerSelection === 'scissors'){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+        return ++playerScore;
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+        return ++playerScore;
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
-        console.log(`You win ${playerSelection} beats ${computerSelection}`);
+        return ++playerScore;
     }
-    else(
-        console.log(`You lost ${computerSelection} beats ${playerSelection}`)
-    )
+    else{
+        return ++computerScore;
+    }
 }
 
+function game() {
 
-console.log(singleGame(playerSelection, computerSelection));
+    for (i = 0; i < 5; i++){
+        playerSelection = prompt("Rock-Paper-Scissors").toLowerCase();
+        singleGame(playerSelection, computerPlay());
+        if(playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissor'){
+            i = i - 1;
+        }
+    }
+        
+    if(playerScore > computerScore) {
+        console.log('win');
+    }
+    else if(playerScore < computerScore){
+        console.log('loser');
+    }
+    else{
+        console.log('draw')
+    }
+}
+console.log(game());

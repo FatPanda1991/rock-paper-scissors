ff7e5d90fa97b3e5f34961998bec9480c1356c05
FatPanda1991 <egidijus8800@gmail.com>
2022-05-01 14:05:37 +0300

    Create new playable code. Make 3 buttons for Rock Paper and Scissors. Score countin till 5 points. Disable buttons after reaching 5 points. Add invisible reset button which apears after reaching 5 points.

--- main.js
+++ main.js
@@ -1,62 +1,148 @@
-const computerChoices = ['rock', 'paper', 'scissors'];
-
-function computerPlay(){
-    let result = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-    return result;
-}
-playerSelection = '';
-let computerSelection = computerPlay();
-let draw = 'Draw';
-let win = `You win ${playerSelection} beats ${computerSelection}`;
-let lost = `You lost ${computerSelection} beats ${playerSelection}`;
 let playerScore = 0;
 let computerScore = 0;
+let resetButton;
 
+function game(playerSelection) {
+    
+    const computerChoices = ['rock', 'paper', 'scissors'];
+    const computerSelection = computerPlay();
 
-
-function singleGame(playerSelection, computerSelection){
+    function computerPlay(){
+    let result = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+    return result;
+    }
     if(playerSelection === computerSelection){
-        console.log("draw");
-        return draw;
+    document.getElementById("result").innerHTML = "draw";
     }
     else if(playerSelection === 'rock' && computerSelection === 'scissors'){
-        document.getElementById("score").innerHTML =`You win ${playerSelection} beats ${computerSelection}`;
-        return ++playerScore;
+    document.getElementById("result").innerHTML = `You win ${playerSelection} beats ${computerSelection}`;
+    ++playerScore;
+    document.getElementById("score").innerHTML = `Your score is ${playerScore} <br> Computer score is ${computerScore}`
+    checkScore()
     }
     else if(playerSelection === 'paper' && computerSelection === 'rock'){
-        document.getElementById("score").innerHTML =`You win ${playerSelection} beats ${computerSelection}`;
-        return ++playerScore;
+    document.getElementById("result").innerHTML = `You win ${playerSelection} beats ${computerSelection}`;
+    ++playerScore;
+    document.getElementById("score").innerHTML = `Your score is ${playerScore} <br> Computer score is ${computerScore}`
+    checkScore()
     }
     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
-        document.getElementById("score").innerHTML =`You win ${playerSelection} beats ${computerSelection}`;
-        return ++playerScore;
+    document.getElementById("result").innerHTML = `You win ${playerSelection} beats ${computerSelection}`;
+    ++playerScore;
+    document.getElementById("score").innerHTML = `Your score is ${playerScore} <br> Computer score is ${computerScore}`
+    checkScore()
     }
     else{
-        document.getElementById("score").innerHTML =`You lost ${computerSelection} beats ${playerSelection}`;
-        return ++computerScore;
+    document.getElementById("result").innerHTML = `You lost ${computerSelection} beats ${playerSelection}`;
+    ++computerScore;
+    document.getElementById("score").innerHTML = `Your score is ${playerScore} <br> Computer score is ${computerScore}`
+    checkScore()
     }
 }
 
-function game() {
-
-    for (i = 0; i < 5; i++){
-        playerSelection = prompt("Rock-Paper-Scissors").toLowerCase(); //Insert player choice and make it lowercase
-        if(playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors'){ //Check if it is correct
-            i = i - 1;
-            console.log(`What is ${playerSelection}`);
-        }
-        else {singleGame(playerSelection, computerPlay()); //If correct play single game
-        }
+function checkScore() {
+    if (playerScore == 5){
+        document.getElementById("outcome").innerHTML = 'You won!';
+        gameOver()
     }
+    else if (computerScore == 5) {
+        document.getElementById("outcome").innerHTML = 'You lost!';
+        gameOver()
+    }
+    else {
+        return
+    }
+}
+
+function gameOver(){
+    document.getElementById("btn-1").disabled = true;
+    document.getElementById("btn-2").disabled = true;
+    document.getElementById("btn-3").disabled = true;
+    document.getElementById("reset").style.display="block"
+}
+
+function resetGame() {
+    document.getElementById("btn-1").disabled = false;
+    document.getElementById("btn-2").disabled = false;
+    document.getElementById("btn-3").disabled = false;
+    playerScore = 0;
+    computerScore = 0;
+    document.getElementById("score").innerHTML = `Your score is ${playerScore} <br> Computer score is ${computerScore}`
+    document.getElementById("reset").style.display="none"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const computerChoices = ['rock', 'paper', 'scissors'];
+
+// function computerPlay(){
+//     let result = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+//     return result;
+// }
+// playerSelection = '';
+// let computerSelection = computerPlay();
+// let draw = 'Draw';
+// let win = `You win ${playerSelection} beats ${computerSelection}`;
+// let lost = `You lost ${computerSelection} beats ${playerSelection}`;
+// let playerScore = 0;
+// let computerScore = 0;
+
+
+
+// function singleGame(playerSelection, computerSelection){
+//     if(playerSelection === computerSelection){
+//         console.log("draw");
+//         return draw;
+//     }
+//     else if(playerSelection === 'rock' && computerSelection === 'scissors'){
+//         console.log(`You win ${playerSelection} beats ${computerSelection}`);
+//         return ++playerScore;
+//     }
+//     else if(playerSelection === 'paper' && computerSelection === 'rock'){
+//         console.log(`You win ${playerSelection} beats ${computerSelection}`);
+//         return ++playerScore;
+//     }
+//     else if(playerSelection === 'scissors' && computerSelection === 'paper'){
+//         console.log(`You win ${playerSelection} beats ${computerSelection}`);
+//         return ++playerScore;
+//     }
+//     else{
+//         console.log(`You lost ${computerSelection} beats ${playerSelection}`);
+//         return ++computerScore;
+//     }
+// }
+
+// function game() {
+
+//     for (i = 0; i < 5; i++){
+//         playerSelection = prompt("Rock-Paper-Scissors").toLowerCase();
+//         singleGame(playerSelection, computerPlay());
+//         if(playerSelection != 'rock' && playerSelection != 'paper' && playerSelection != 'scissors'){
+//             i = i - 1;
+//         }
+//     }
         
-    if(playerScore > computerScore) { //count scores and declare winner
-        document.getElementById("result").innerHTML = `You won with ${playerScore} points against computer with ${computerScore} points`;
-    }
-    else if(playerScore < computerScore){
-        document.getElementById("result").innerHTML = `You lost with ${playerScore} points against computer with ${computerScore} points`;
-    }
-    else{
-        document.getElementById("result").innerHTML = `It is draw. You both have ${playerScore} points`;
-    }
-}
-
+//     if(playerScore > computerScore) {
+//         console.log('win');
+//     }
+//     else if(playerScore < computerScore){
+//         console.log('loser');
+//     }
+//     else{
+//         console.log('draw')
+//     }

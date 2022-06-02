53a927ea9d1ea321b4bc6ebe7a727b3dfa79931c
FatPanda1991 <egidijus8800@gmail.com>
2022-06-02 23:49:24 +0300

    Add win/lose messages, fixed css layout, minor js fixes

--- main.js
+++ main.js
@@ -11,53 +11,55 @@ function game(playerSelection) {
 
     function computerPlay(){
         let result = computerChoices[Math.floor(Math.random()*computerChoices.length)];
-        document.getElementById(`${result}`).style.boxShadow = "0px 0px 12px 4px rgb(219, 62, 22)";
+        document.getElementById(`${result}`).style.boxShadow = "0px 0px 12px 4px rgb(212, 0, 0)";
         return result;
     }
     if(playerSelection === computerSelection){
-        document.getElementById("result").innerHTML = "Draw";
+        document.getElementById("result").innerHTML += "<br>Draw<br>";
     }
     else if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
-        document.getElementById("result").innerHTML = `You win ${playerSelection} 
-             beats ${computerSelection}`;
+        document.getElementById("result").innerHTML += `<br>You win ${playerSelection} 
+             beats ${computerSelection}<br>`;
         ++playerScore;
-        document.getElementById("score").innerHTML = `Your score is ${playerScore} 
-            <br> Computer score is ${computerScore}`
+        document.getElementById("compScore").innerHTML = `${computerScore}`;
+        document.getElementById("plrScore").innerHTML = `${playerScore}`;
         checkScore()
     }
     else if(playerSelection === 'Paper' && computerSelection === 'Rock'){
-        document.getElementById("result").innerHTML = `You win ${playerSelection} 
-            beats ${computerSelection}`;
+        document.getElementById("result").innerHTML += `<br>You win ${playerSelection} 
+            beats ${computerSelection}<br>`;
         ++playerScore;
-        document.getElementById("score").innerHTML = `Your score is ${playerScore} 
-            <br> Computer score is ${computerScore}`
+        document.getElementById("compScore").innerHTML = `${computerScore}`;
+        document.getElementById("plrScore").innerHTML = `${playerScore}`;
         checkScore()
     }
     else if(playerSelection === 'Scissors' && computerSelection === 'Paper'){
-        document.getElementById("result").innerHTML = `You win ${playerSelection} 
-            beats ${computerSelection}`;
+        document.getElementById("result").innerHTML += `<br>You win ${playerSelection} 
+            beats ${computerSelection}<br>`;
         ++playerScore;
-        document.getElementById("score").innerHTML = `Your score is ${playerScore} 
-            <br> Computer score is ${computerScore}`
+        document.getElementById("compScore").innerHTML = `${computerScore}`;
+        document.getElementById("plrScore").innerHTML = `${playerScore}`;
         checkScore()
     }
     else{
-        document.getElementById("result").innerHTML = `You lost ${computerSelection} 
-            beats ${playerSelection}`;
+        document.getElementById("result").innerHTML += `<br>You lost ${computerSelection} 
+            beats ${playerSelection}<br>`;
         ++computerScore;
-        document.getElementById("score").innerHTML = `Your score is ${playerScore} 
-            <br> Computer score is ${computerScore}`
+        document.getElementById("compScore").innerHTML = `${computerScore}`;
+        document.getElementById("plrScore").innerHTML = `${playerScore}`;
         checkScore()
     }
 }
 
 function checkScore() {
     if (playerScore == 5){
-        document.getElementById("outcome").innerHTML = 'You won!';
+        document.getElementById("result").classList.add("win");
+        document.getElementById("result").innerHTML = 'You won!';
         gameOver()
     }
     else if (computerScore == 5) {
-        document.getElementById("outcome").innerHTML = 'You lost!';
+        document.getElementById("result").classList.add("lose");
+        document.getElementById("result").innerHTML = 'You lost!';
         gameOver()
     }
     else {
@@ -73,6 +75,8 @@ function gameOver(){
 }
 
 function resetGame() {
+    document.getElementById("result").classList.remove("win");
+    document.getElementById("result").classList.remove("lose");
     document.getElementById(`Rock`).style.boxShadow = null;
     document.getElementById(`Paper`).style.boxShadow = null;
     document.getElementById(`Scissors`).style.boxShadow = null;
@@ -81,23 +85,30 @@ function resetGame() {
     document.getElementById("btn-3").disabled = false;
     playerScore = 0;
     computerScore = 0;
-    document.getElementById("score").innerHTML = `Your score is ${playerScore} 
-        <br> Computer score is ${computerScore}`
-    document.getElementById("reset").style.display="none"
+    document.getElementById("result").innerHTML = "";
+    document.getElementById("reset").style.display="none";
+    document.getElementById("plrScore").innerHTML="0";
+    document.getElementById("compScore").innerHTML="0";
 }
 
 
 function gameInf(playerSelection) {
+    document.getElementById(`Rock`).style.boxShadow = null;
+    document.getElementById(`Paper`).style.boxShadow = null;
+    document.getElementById(`Scissors`).style.boxShadow = null;
     
     const computerChoices = ['Rock', 'Paper', 'Scissors'];
     const computerSelection = computerPlay();
 
     function computerPlay(){
         let result = computerChoices[Math.floor(Math.random()*computerChoices.length)];
+        document.getElementById(`${result}`).style.boxShadow = "0px 0px 12px 4px rgb(212, 0, 0);";
             return result;
     }
     if(playerSelection === computerSelection){
         document.getElementById("result").innerHTML = "Draw";
+        document.getElementById("score").innerHTML = `Your score is ${playerScore}
+             <br> Computer score is ${computerScore}`
     }
     else if(playerSelection === 'Rock' && computerSelection === 'Scissors'){
         document.getElementById("result").innerHTML = `You win ${playerSelection} 
